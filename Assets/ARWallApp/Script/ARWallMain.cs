4c2008d87d80a13752e0e0082e771cb3399064bd
hsinpa <hsinpa@gmail.com>
2018-11-10 21:02:48 +0800

    Done with the first of screen capture, and sending email

---
+++ Assets/ARWallApp/Script/ARWallMain.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ARWallMain : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		EmailManager email = transform.Find("Service/Email").GetComponent<EmailManager>();
+		CameraScreenCapture cameraCapture = transform.Find("Service/ScreenShot").GetComponent<CameraScreenCapture>();
+
+		cameraCapture.SetUp(Camera.main);
+		email.SetUp(cameraCapture);
+	}
+
+}
--- Assets/ARWallApp/Script/Email/EmailService.cs
+++ Assets/ARWallApp/Script/Email/EmailService.cs
@@ -4,6 +4,7 @@ using System.Net;
 using System.Net.Mail;
 using System.Net.Security;
 using System.Security.Cryptography.X509Certificates;
+using System.IO;
 
 public class EmailService  {
 	private string _email, _password, _name, _stmpAddress;
@@ -17,27 +18,35 @@ public class EmailService  {
 		this._port = 587;
 	}
 
-	public void Send(string p_subject, string p_content, string[] p_targetEmails) {
-		MailMessage mail = CreateMail("HelloPaul", "Thankful", _email, _name, p_targetEmails);
+	public void Send(string p_subject, string p_content, string[] p_targetEmails, string[] p_attachmentDir) {
+		MailMessage mail = CreateMail(p_subject, p_content, _email, _name, p_targetEmails, p_attachmentDir);
 		SmtpClient smtp = CreateSMTP(_email, _password, _stmpAddress);
 
-		// smtp.Send(mail);
+		smtp.SendAsync(mail, "user states . . .");
 	}
 
-	private MailMessage CreateMail(string p_subject, string p_content, string p_email, string p_name, string[] p_targetEmails) {
+	private MailMessage CreateMail(string p_subject, string p_content, string p_email, string p_name, 
+		string[] p_targetEmails, string[] attachmentPics) {
 		MailMessage mail = new MailMessage();
 		
         mail.From = new MailAddress (p_email, p_name, System.Text.Encoding.UTF8);
-
+		
+		//Add target
 		foreach (string targetEmail in p_targetEmails) {
 			mail.To.Add (targetEmail);
+		}
+		
+		//Add attachment
+		foreach (string attachment in attachmentPics) {
+			if (File.Exists(attachment)) {
+				mail.Attachments.Add (new Attachment (@attachment) );
+			}
 		}
 
         mail.Subject = p_subject;
         mail.Body = p_content;
         mail.SubjectEncoding = System.Text.Encoding.UTF8;
         mail.BodyEncoding = System.Text.Encoding.UTF8;
-        // mail.Attachments.Add (attachment);
         mail.Priority = MailPriority.High;
 
 		return mail;
@@ -45,7 +54,7 @@ public class EmailService  {
 
 	private SmtpClient CreateSMTP(string p_email, string p_password, string p_smtpAddress) {
 		    SmtpClient smtpServer = new SmtpClient(p_smtpAddress);
-			smtpServer.Port = 465;
+			smtpServer.Port = 587;
 			smtpServer.Credentials = new System.Net.NetworkCredential (p_email, p_password) as ICredentialsByHost;
 			smtpServer.EnableSsl = true;
 			ServicePointManager.ServerCertificateValidationCallback = 
